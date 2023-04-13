d6f44ce5ae667e77ea390adad53e992a9f3db341
Green Sky <green@g-s.xyz>
2023-04-13 17:54:19 +0200

    events call lua

--- src/tox_lua_module.cpp
+++ src/tox_lua_module.cpp
@@ -6,6 +6,7 @@
 #include <LuaBridge3/LuaBridge.h>
 
 #include <fstream>
+#include <type_traits>
 
 #define REG_ENUM(x) template<> struct luabridge::Stack<x> : luabridge::Enum<x> {};
 
@@ -174,11 +175,438 @@ ToxLuaModule::ToxLuaModule(ToxI& t, ToxEventProviderI& tep) : _t(t) {
 		// once executed, can we free it? (unique_ptr frees here)
 	}
 
-	// TODO: all?
-	tep.subscribe(this, Tox_Event::TOX_EVENT_FRIEND_MESSAGE);
-	tep.subscribe(this, Tox_Event::TOX_EVENT_GROUP_MESSAGE);
+	tep.subscribe(this, TOX_EVENT_SELF_CONNECTION_STATUS);
+
+	tep.subscribe(this, TOX_EVENT_FRIEND_REQUEST);
+	tep.subscribe(this, TOX_EVENT_FRIEND_CONNECTION_STATUS);
+	tep.subscribe(this, TOX_EVENT_FRIEND_LOSSY_PACKET);
+	tep.subscribe(this, TOX_EVENT_FRIEND_LOSSLESS_PACKET);
+
+	tep.subscribe(this, TOX_EVENT_FRIEND_NAME);
+	tep.subscribe(this, TOX_EVENT_FRIEND_STATUS);
+	tep.subscribe(this, TOX_EVENT_FRIEND_STATUS_MESSAGE);
+
+	tep.subscribe(this, TOX_EVENT_FRIEND_MESSAGE);
+	tep.subscribe(this, TOX_EVENT_FRIEND_READ_RECEIPT);
+	tep.subscribe(this, TOX_EVENT_FRIEND_TYPING);
+
+	tep.subscribe(this, TOX_EVENT_FILE_CHUNK_REQUEST);
+	tep.subscribe(this, TOX_EVENT_FILE_RECV);
+	tep.subscribe(this, TOX_EVENT_FILE_RECV_CHUNK);
+	tep.subscribe(this, TOX_EVENT_FILE_RECV_CONTROL);
+
+	tep.subscribe(this, TOX_EVENT_CONFERENCE_INVITE);
+	tep.subscribe(this, TOX_EVENT_CONFERENCE_CONNECTED);
+	tep.subscribe(this, TOX_EVENT_CONFERENCE_PEER_LIST_CHANGED);
+	tep.subscribe(this, TOX_EVENT_CONFERENCE_PEER_NAME);
+	tep.subscribe(this, TOX_EVENT_CONFERENCE_TITLE);
+
+	tep.subscribe(this, TOX_EVENT_CONFERENCE_MESSAGE);
+
+	tep.subscribe(this, TOX_EVENT_GROUP_PEER_NAME);
+	tep.subscribe(this, TOX_EVENT_GROUP_PEER_STATUS);
+	tep.subscribe(this, TOX_EVENT_GROUP_TOPIC);
+	tep.subscribe(this, TOX_EVENT_GROUP_PRIVACY_STATE);
+	tep.subscribe(this, TOX_EVENT_GROUP_VOICE_STATE);
+	tep.subscribe(this, TOX_EVENT_GROUP_TOPIC_LOCK);
+	tep.subscribe(this, TOX_EVENT_GROUP_PEER_LIMIT);
+	tep.subscribe(this, TOX_EVENT_GROUP_PASSWORD);
+	tep.subscribe(this, TOX_EVENT_GROUP_MESSAGE);
+	tep.subscribe(this, TOX_EVENT_GROUP_PRIVATE_MESSAGE);
+	tep.subscribe(this, TOX_EVENT_GROUP_CUSTOM_PACKET);
+	tep.subscribe(this, TOX_EVENT_GROUP_CUSTOM_PRIVATE_PACKET);
+	tep.subscribe(this, TOX_EVENT_GROUP_INVITE);
+	tep.subscribe(this, TOX_EVENT_GROUP_PEER_JOIN);
+	tep.subscribe(this, TOX_EVENT_GROUP_PEER_EXIT);
+	tep.subscribe(this, TOX_EVENT_GROUP_SELF_JOIN);
+	tep.subscribe(this, TOX_EVENT_GROUP_JOIN_FAIL);
+	tep.subscribe(this, TOX_EVENT_GROUP_MODERATION);
 }
 
 ToxLuaModule::~ToxLuaModule(void) {
 }
+
+#if 0
+template<typename EventT, typename FN>
+auto callEventArgs(const EventT* e, FN&& fn) {
+	if constexpr (std::is_same_v<EventT, Tox_Event_Self_Connection_Status>) {
+		return fn(
+			tox_event_self_connection_status_get_connection_status(e)
+		);
+	} // else if ...
+}
+
+#else
+//template<typename EventT, typename FN>
+//auto callEventArgs(const EventT* e, FN&& fn) { return fn(); }
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Self_Connection_Status* e, FN&& fn) {
+	return fn(
+		tox_event_self_connection_status_get_connection_status(e)
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Conference_Connected* e, FN&& fn) {
+	return fn(
+		tox_event_conference_connected_get_conference_number(e)
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Conference_Invite* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Conference_Message* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Conference_Peer_List_Changed* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Conference_Peer_Name* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Conference_Title* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_File_Chunk_Request* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_File_Recv* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_File_Recv_Chunk* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_File_Recv_Control* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Connection_Status* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Lossless_Packet* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Lossy_Packet* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Message* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Name* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Read_Receipt* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Request* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Status* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Status_Message* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Friend_Typing* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Peer_Name* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Peer_Status* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Topic* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Privacy_State* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Voice_State* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Topic_Lock* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Peer_Limit* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Password* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Message* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Private_Message* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Custom_Packet* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Custom_Private_Packet* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Invite* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Peer_Join* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Peer_Exit* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Self_Join* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Join_Fail* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+template<typename FN>
+auto callEventArgs(const Tox_Event_Group_Moderation* e, FN&& fn) {
+	return fn(
+
+	);
+}
+
+#endif
+
+bool ToxLuaModule::onToxEvent(const Tox_Event_Self_Connection_Status* e) {
+	// get lua function
+	luabridge::LuaRef g_events_table = luabridge::getGlobal(_lua_state_global.get(), "TOX_EVENTS");
+	if (!g_events_table.isTable()) {
+		std::cerr << "TLM waring: global table TOX_EVENTS not set\n";
+		return false;
+	}
+
+	auto lr_fn = g_events_table["Tox_Event_Self_Connection_Status"];
+	if (!lr_fn.isCallable()) {
+		std::cerr << "TLM waring: Tox_Event_Self_Connection_Status is not callable\n";
+		return false;
+	}
+
+	// call
+	auto res = callEventArgs(e, lr_fn);
+
+	if (res.hasFailed() || res.size() != 1) {
+		std::cerr << "TLM error, Tox_Event_Self_Connection_Status callback failed\n";
+		return false;
+	}
+
+	if (!res[0].isBool()) {
+		std::cerr << "TLM error, Tox_Event_Self_Connection_Status callback did not return a bool\n";
+		return false;
+	}
+
+	return static_cast<bool>(res[0]);
+}
+
+#define EVENT_IMPL(x) \
+bool ToxLuaModule::onToxEvent(const x* e) { \
+	luabridge::LuaRef g_events_table = luabridge::getGlobal(_lua_state_global.get(), "TOX_EVENTS"); \
+	if (!g_events_table.isTable()) { \
+		std::cerr << "TLM waring: global table TOX_EVENTS not set\n"; \
+		return false; \
+	} \
+	auto lr_fn = g_events_table[#x]; \
+	if (!lr_fn.isCallable()) { \
+		return false; \
+	} \
+	auto res = callEventArgs(e, lr_fn); \
+	if (res.hasFailed() || res.size() != 1) { \
+		std::cerr << "TLM error, " #x " callback failed\n"; \
+		return false; \
+	} \
+	if (!res[0].isBool()) { \
+		std::cerr << "TLM error, " #x " callback did not return a bool\n"; \
+		return false; \
+	} \
+	return static_cast<bool>(res[0]); \
+}
+
+EVENT_IMPL(Tox_Event_Conference_Connected)
+EVENT_IMPL(Tox_Event_Conference_Invite)
+EVENT_IMPL(Tox_Event_Conference_Message)
+EVENT_IMPL(Tox_Event_Conference_Peer_List_Changed)
+EVENT_IMPL(Tox_Event_Conference_Peer_Name)
+EVENT_IMPL(Tox_Event_Conference_Title)
+
+EVENT_IMPL(Tox_Event_File_Chunk_Request)
+EVENT_IMPL(Tox_Event_File_Recv)
+EVENT_IMPL(Tox_Event_File_Recv_Chunk)
+EVENT_IMPL(Tox_Event_File_Recv_Control)
+
+EVENT_IMPL(Tox_Event_Friend_Connection_Status)
+EVENT_IMPL(Tox_Event_Friend_Lossless_Packet)
+EVENT_IMPL(Tox_Event_Friend_Lossy_Packet)
+EVENT_IMPL(Tox_Event_Friend_Message)
+EVENT_IMPL(Tox_Event_Friend_Name)
+EVENT_IMPL(Tox_Event_Friend_Read_Receipt)
+EVENT_IMPL(Tox_Event_Friend_Request)
+EVENT_IMPL(Tox_Event_Friend_Status)
+EVENT_IMPL(Tox_Event_Friend_Status_Message)
+EVENT_IMPL(Tox_Event_Friend_Typing)
+
+EVENT_IMPL(Tox_Event_Group_Peer_Name)
+EVENT_IMPL(Tox_Event_Group_Peer_Status)
+EVENT_IMPL(Tox_Event_Group_Topic)
+EVENT_IMPL(Tox_Event_Group_Privacy_State)
+EVENT_IMPL(Tox_Event_Group_Voice_State)
+EVENT_IMPL(Tox_Event_Group_Topic_Lock)
+EVENT_IMPL(Tox_Event_Group_Peer_Limit)
+EVENT_IMPL(Tox_Event_Group_Password)
+EVENT_IMPL(Tox_Event_Group_Message)
+EVENT_IMPL(Tox_Event_Group_Private_Message)
+EVENT_IMPL(Tox_Event_Group_Custom_Packet)
+EVENT_IMPL(Tox_Event_Group_Custom_Private_Packet)
+EVENT_IMPL(Tox_Event_Group_Invite)
+EVENT_IMPL(Tox_Event_Group_Peer_Join)
+EVENT_IMPL(Tox_Event_Group_Peer_Exit)
+EVENT_IMPL(Tox_Event_Group_Self_Join)
+EVENT_IMPL(Tox_Event_Group_Join_Fail)
+EVENT_IMPL(Tox_Event_Group_Moderation)
 
--- src/tox_lua_module.hpp
+++ src/tox_lua_module.hpp
@@ -19,5 +19,55 @@ class ToxLuaModule : public ToxEventI {
 		ToxLuaModule(ToxI& t, ToxEventProviderI& tep);
 		~ToxLuaModule(void);
 
+	protected: // tox events
+
+#define OVER_EVENT(x) bool onToxEvent(const x*) override;
+
+	OVER_EVENT(Tox_Event_Conference_Connected)
+	OVER_EVENT(Tox_Event_Conference_Invite)
+	OVER_EVENT(Tox_Event_Conference_Message)
+	OVER_EVENT(Tox_Event_Conference_Peer_List_Changed)
+	OVER_EVENT(Tox_Event_Conference_Peer_Name)
+	OVER_EVENT(Tox_Event_Conference_Title)
+
+	OVER_EVENT(Tox_Event_File_Chunk_Request)
+	OVER_EVENT(Tox_Event_File_Recv)
+	OVER_EVENT(Tox_Event_File_Recv_Chunk)
+	OVER_EVENT(Tox_Event_File_Recv_Control)
+
+	OVER_EVENT(Tox_Event_Friend_Connection_Status)
+	OVER_EVENT(Tox_Event_Friend_Lossless_Packet)
+	OVER_EVENT(Tox_Event_Friend_Lossy_Packet)
+	OVER_EVENT(Tox_Event_Friend_Message)
+	OVER_EVENT(Tox_Event_Friend_Name)
+	OVER_EVENT(Tox_Event_Friend_Read_Receipt)
+	OVER_EVENT(Tox_Event_Friend_Request)
+	OVER_EVENT(Tox_Event_Friend_Status)
+	OVER_EVENT(Tox_Event_Friend_Status_Message)
+	OVER_EVENT(Tox_Event_Friend_Typing)
+
+	OVER_EVENT(Tox_Event_Self_Connection_Status)
+
+	OVER_EVENT(Tox_Event_Group_Peer_Name)
+	OVER_EVENT(Tox_Event_Group_Peer_Status)
+	OVER_EVENT(Tox_Event_Group_Topic)
+	OVER_EVENT(Tox_Event_Group_Privacy_State)
+	OVER_EVENT(Tox_Event_Group_Voice_State)
+	OVER_EVENT(Tox_Event_Group_Topic_Lock)
+	OVER_EVENT(Tox_Event_Group_Peer_Limit)
+	OVER_EVENT(Tox_Event_Group_Password)
+	OVER_EVENT(Tox_Event_Group_Message)
+	OVER_EVENT(Tox_Event_Group_Private_Message)
+	OVER_EVENT(Tox_Event_Group_Custom_Packet)
+	OVER_EVENT(Tox_Event_Group_Custom_Private_Packet)
+	OVER_EVENT(Tox_Event_Group_Invite)
+	OVER_EVENT(Tox_Event_Group_Peer_Join)
+	OVER_EVENT(Tox_Event_Group_Peer_Exit)
+	OVER_EVENT(Tox_Event_Group_Self_Join)
+	OVER_EVENT(Tox_Event_Group_Join_Fail)
+	OVER_EVENT(Tox_Event_Group_Moderation)
+
+#undef OVER_EVENT
+
 };
 
